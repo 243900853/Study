b984936e70f9739d5c46735099a6e10879bf9346
243900853 <243900853@qq.com>
2019-11-26 23:12:54 +0800

    spring aop (一）（1） 57:16

--- spring-framework-5.1.x/spring-xb-test/src/main/java/com/xiaobi/CGLib/FamilyAopBeanPostProcessor.java
+++ spring-framework-5.1.x/spring-xb-test/src/main/java/com/xiaobi/CGLib/FamilyAopBeanPostProcessor.java
@@ -7,7 +7,8 @@ import com.xiaobi.service.XBService1;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.cglib.proxy.Enhancer;
-
+//在Appconfig里面用@Import导入，所以Spring才扫描出FamilyAopBeanPostProcessor
+//不能用@Component来注入,因为如果以后这个后置处理器给其他程序员扩展使用,要让程序员扫描这个后置处理器所在的包才能使用
 public class FamilyAopBeanPostProcessor implements BeanPostProcessor {
 	@Override
 	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
